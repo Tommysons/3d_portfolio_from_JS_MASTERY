0869d9982e71b027d80bc575c4de9a35f60f4058
Tudish <lokatorist2@inbox.lv>
2024-04-12 08:02:28 +0300

    Have create a scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
-import { Footer, Navbar } from "./components";
-import { About, Contact, Home, Projects } from "./pages";
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import {Home, About, Projects, Contact} from './pages'
 
 const App = () => {
   return (
-    <main className='bg-slate-300/20'>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route
-            path='/*'
-            element={
-              <>
-                <Routes>
-                  <Route path='/about' element={<About />} />
-                  <Route path='/projects' element={<Projects />} />
-                  <Route path='/contact' element={<Contact />} />
-                </Routes>
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
-      </Router>
+    <main className='bh-slate-300/20'>
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/projects' element={<Projects/>}/>
+                <Route path='/contact' element={<Contact/>}/>
+            </Routes>
+        </Router>
     </main>
-  );
-};
+  )
+}
 
-export default App;
+export default App
