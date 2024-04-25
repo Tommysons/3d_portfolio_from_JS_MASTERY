6d3417213f081ad3a28bf07c8178750b774f1f46
Tudish <lokatorist2@inbox.lv>
2024-04-25 23:04:18 +0300

    Have done HomeInfo

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <section className='relative flex lg:flex-row flex-col
+    max-container'>
+      <div className='w-full lg:w-1/2 flex flex-col justify-center'>sss</div>
+    </section>
   )
 }
 
 export default Contact
-Contact
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,10 @@ import Island from '../models/Island'
 import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
-//1.27.12
+import HomeInfo from '../components/HomeInfo'
 
-//saeaewqe
-{/* <div className='absolute top-28 left-0 right-0 z-10 flex
-items-center justify-center'>
-  POPOP
-</div> */}
+
+
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false)
@@ -48,6 +45,10 @@ const Home = () => {
   const [planeScale, planePosition] = adjustPlaneForScreenSize()
   return (
     <section className='w-full h-screen relative'>
+      <div className='absolute top-28 left-0 right-0 z-10 flex
+      items-center justify-center'>
+        {currentStage && <HomeInfo currentStage={currentStage}/>}
+      </div> 
       <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
       camera={{near: 0.1, far: 1000}}>
         <Suspense fallback={<Loader/>}>
